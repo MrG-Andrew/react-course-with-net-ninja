dece04fe450bf81d9df31d32ad96d1d4f654f556
andrew <alm3allmandy@gmail.com>
2021-11-08 19:55:57 +0200

    learned props and used useState to update variables

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+
+    const[blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'andrew', id: 1},
+        { title: 'welcome party!', body: 'lorem ipsum...', author: 'shery', id: 2},
+        { title: 'web developer', body: 'lorem ipsum...', author: 'essam', id: 3}
+    ]);
+
+    const handleDelete  = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !==  id);
+        setBlogs(newBlogs);
+    }
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+           <BlogList blogs = {blogs} title = "All Blogs!!" handleDelete = {handleDelete}/>
+           
         </div>
      );
 }
