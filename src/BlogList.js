888b051b5b3dfca6f54ae8375e92d952d8832696
andrew <alm3allmandy@gmail.com>
2021-11-09 20:58:48 +0200

    adding fetch data from db and catching errors

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,13 +1,12 @@
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs}) => {
     
     return ( 
         <div className="blog-list">
-            <h2>{title}</h2>
             {blogs.map((blog) => (
             <div className="blog-preview" key = {blog.id}>
                 <h2>{blog.title}</h2>
                 <p>written by: {blog.author}</p>
-                <button onClick={() => handleDelete(blog.id) } >delete blog</button>
+                
             </div>
            ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const[blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'andrew', id: 1},
-        { title: 'welcome party!', body: 'lorem ipsum...', author: 'shery', id: 2},
-        { title: 'web developer', body: 'lorem ipsum...', author: 'essam', id: 3}
-    ]);
+    const[blogs, setBlogs] = useState([]);
 
-    const handleDelete  = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !==  id);
-        setBlogs(newBlogs);
-    }
+    const[loading, setLoading] = useState(true);
+
+    const[error, setError] = useState(null)
+
+    useEffect(()=>{
+        setTimeout(()=>{
+        fetch('http://localhost:4000/blogs')
+            .then(res => {
+                if(!res.ok){
+                    throw Error('couldn\'t fetch the data for that resourse');
+                };
+                return res.json();
+            })
+            .then(data => {
+                setLoading(false);
+                setBlogs(data);
+                setError(null);
+            })
+            .catch(err => {
+                setLoading(false);
+                setError(err.message);
+            })
+        },1000);
+    },[])
+
     return ( 
         <div className="home">
-           <BlogList blogs = {blogs} title = "All Blogs!!" handleDelete = {handleDelete}/>
+            {error && <div>{error}</div>}
+            {loading && <div>Loading...</div>}
+            <BlogList blogs = {blogs} />
            
         </div>
      );
